60906b831d1e21c7eb6cbd56f2836f4331719728
Marcio 1995 Noronha <marcionoronha1995@gmail.com>
2023-02-22 21:46:06 -0300

    Update myScript.js

--- Exercicio1parte2/src/myScript.js
+++ Exercicio1parte2/src/myScript.js
@@ -1,10 +1,10 @@
-const fototitulo = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/Exercicio2v2/img/pexels-l%C3%BAcio-arantes-3656636.jpg";
-const foto1 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/Exercicio2v2/img/IMG_20230216_205400_2.jpg";
-const foto2 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/Exercicio2v2/img/meadow-3628843__340.jpg";
-const foto3 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/Exercicio2v2/img/living-room-2732939__340%20(1).jpg";
-const foto4 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/Exercicio2v2/img/living-room-2569325__340.jpg";
-const foto5 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/Exercicio2v2/img/bedroom-1872196__340%20(1).jpg";
-const foto6 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/Exercicio2v2/img/blooming-field-5304878__340.jpg";
+const fototitulo = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/EXERCICIO1PARTE2/img/pexels-l%C3%BAcio-arantes-3656636.jpg";
+const foto1 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/EXERCICIO1PARTE2/img/IMG_20230216_205400_2.jpg";
+const foto2 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/EXERCICIO1PARTE2/img/meadow-3628843__340.jpg";
+const foto3 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/EXERCICIO1PARTE2/img/living-room-2732939__340%20(1).jpg";
+const foto4 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/EXERCICIO1PARTE2/img/living-room-2569325__340.jpg";
+const foto5 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/EXERCICIO1PARTE2/img/bedroom-1872196__340%20(1).jpg";
+const foto6 = "https://raw.githubusercontent.com/marcionoronha1995/futurodev2023/main/EXERCICIO1PARTE2/img/blooming-field-5304878__340.jpg";
 
 document.getElementById('imagem1').src=foto1;
 document.getElementById('imagem2').src=foto2;
