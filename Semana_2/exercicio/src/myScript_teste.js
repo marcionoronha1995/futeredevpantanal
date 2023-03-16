4d5ee099368cb03d6322c610e136bd56cb66e8df
Marcio 1995 Noronha <marcionoronha1995@gmail.com>
2023-03-16 07:00:28 -0300

    ainda com problema no push

---
+++ Semana_2/exercicio/src/myScript_teste.js
@@ -0,0 +1,10 @@
+const obj_dados = {peso:""}
+function passarpeso() = ({
+    
+    obj_dados.peso = document.getElementById('weight').value
+    const arr_dados = []
+    arr_dados.push(obj_dados)
+    console.table(obj_dados)
+    console.table(arr_dados)
+
+})
