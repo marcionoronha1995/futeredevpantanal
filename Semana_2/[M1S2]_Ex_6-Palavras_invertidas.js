2fd99c9f32ae1209dd971d062dd2230d5e68063b
Marcio 1995 Noronha <marcionoronha1995@gmail.com>
2023-03-26 08:48:44 -0300

    pronnto

--- Semana_2/[M1S2]_Ex_6-Palavras_invertidas.js
+++ Semana_2/[M1S2]_Ex_6-Palavras_invertidas.js
@@ -6,6 +6,7 @@
 
 const arraypalavras = [];
 const arrayletras = [];
+const arrayinvert = [];
 let i;
 
 function gravaletra(uletra){
@@ -22,12 +23,21 @@ function gravapalavra(uletra){
     document.getElementById("palavra_gravada").innerHTML=palavra;
     console.log("array "+arraypalavras.length);
     console.log("palavra "+palavra+" tem "+palavra.length+" letras");
-    i = palavra.length;
+    i = 0;
+    ii = 0;
+    pp = palavra.length - 1;
     for (let index = 0; index < palavra.length; index++) {
-        const element = palavra[i];
-        console.log("dentro do for "+i--);
-        
-    }
+        const element = palavra[ii];
+        console.log("no for a palavra "+i+" element "+element);        
+        arrayinvert[i]=palavra[pp];
+        console.log("no for a inverção "+arrayinvert[i]);
+        ii++;
+        i++;
+        pp--;        
+    };
+    console.log("invertendo a palavra - "+palavra+" - fica - "+arrayinvert.join(""));
+    document.getElementById("palavra_invertida").innerHTML = arrayinvert.join("");
+
 }
 
 //interando com for...
