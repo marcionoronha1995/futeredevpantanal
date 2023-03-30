e9f2e9d606642e9b5bb9abfb28cd022a27068876
Marcio 1995 Noronha <marcionoronha1995@gmail.com>
2023-03-30 02:30:50 -0300

    new

--- Semana_2/[M1S2]Ex8-Polígono-Regular.js
+++ Semana_2/[M1S2]Ex8-Polígono-Regular.js
@@ -4,33 +4,44 @@
 // Deve-se calcular e colocar em uma condicional if ou switch/case  e estas devem consolar o seguinte:
 // Requisitos: tr
 // 1 - número de lados de um polígono regular  
-let numerolado = 0;
+let numerolado = document.getElementById('numerolados').value;
 // 2 - medida do lado (em cm). 
-let medidalado = 0;
+let poligono;
 
 
 function triangulo(numerolado) {
-  switch (true){
-    case (numerolado < 3): 
-        // Caso o número de lados seja inferior a 3 apresentar no console "não é polígono"..
-        console.log("não é polígono");
-        break;
-    case (numerolado = 3):
+    
+  switch(numerolado){
+    // case (numerolado < 3): 
+    //     // Caso o número de lados seja inferior a 3 apresentar no console "não é polígono"..
+    //     console.log("não é polígono");
+    //     poligono = "\<br>\...é um polígono de "+numerolado+" lados."        
+    //     break;
+    case 3:
         // Se o número de lados for igual a 3 apresentar no console "triângulo" e o valor da área
         console.log("número de lados "+numerolado);
+        poligono = "\<br>\...é um polígono de "+numerolado+" lados."
+        document.getElementById("html1").innerHTML=("valor de numerolado "+numerolado+poligono);
         break;
-    case (numerolado = 4):
+    case 4:
         // Se o número de lados for igual a 4 apresentar no console "quadrado" e o valor da área.
         console.log("número de lados "+numerolado);
+        poligono = "\<br>\...é um polígono de "+numerolado+" lados."
+        document.getElementById("html1").innerHTML=("valor de numerolado "+numerolado+poligono);
+        break;
+    case 5:
+        // Se o número de lados for igual a 5 apresentar no console "pentágono" e o valor da área.
+        console.log("número de lados "+numerolado);
+        poligono = "\<br>\...é um polígono de "+numerolado+" lados."
+        document.getElementById("html1").innerHTML=("valor de numerolado "+numerolado+poligono);
         break;
     default:
         console.log("fora de escopo");
+        poligono = "\<br>...Não se se é um polígono";
+        document.getElementById("html1").innerHTML=("valor de numerolado "+numerolado+poligono);
         break;
-    // case (numerolado = 5):
-    //     // Se o número de lados for igual a 5 apresentar no console "pentágono" e o valor da área.
-    //     console.log("número de lados "+numerolado);
-    //     break;
-}
-  console.log("valor de numerolado "+numerolado)
-  numerolado = 0;
+    }
+   numerolado = 0;
 };
+
+
