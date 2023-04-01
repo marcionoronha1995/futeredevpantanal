63dd59f2f81d1edb9e394189ed5db94d72e5afe2
Marcio 1995 Noronha <marcionoronha1995@gmail.com>
2023-04-01 15:18:24 -0300

    semana3ex1feito

---
+++ Semana_3/[MxSyy]Ex1-ParouImpar.js
@@ -0,0 +1,21 @@
+// Crie um código javascript que checa com estruturas condicionais (if - else) se um determinado número é par ou ímpar. Lembre-se de imprimir o resultado na tela! 
+
+console.log("at rum");
+let ah = (prompt('Informe o número por favor:'));
+
+    const resultado = (ah/2) % 2 == 0
+    let bh
+    var resto = ah % 2;
+
+    if (resto == 0) {
+        bh = "número "+ah+" é par!"
+
+    } else {
+        bh = "número "+ah+" é impar!"
+    }
+
+
+
+const pagina = ["Você digitou "+ah,bh];
+
+alert(pagina);
