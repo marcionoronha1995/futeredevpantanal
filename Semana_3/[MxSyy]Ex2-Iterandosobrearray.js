4ef7c10d13ac85661c8daf0e7eb79ed029f34bdb
Marcio 1995 Noronha <marcionoronha1995@gmail.com>
2023-04-01 15:24:07 -0300

    pronto

---
+++ Semana_3/[MxSyy]Ex2-Iterandosobrearray.js
@@ -0,0 +1,9 @@
+// Crie um código javascript que imprime no console cada número de uma lista (array) de 10 números. 
+
+let i = 0;
+const array = [1,2,3,4,5,6,7,8,9,10];
+array.forEach(element => {
+    console.log(array[i]);
+    i++;
+    
+});
