b82da7bf4ef3cc82f57df68ed864314b9aa6aa2d
Marcio 1995 Noronha <marcionoronha1995@gmail.com>
2023-04-01 15:42:33 -0300

    pront4

---
+++ Semana_3/[MxSyy]Ex4-Funções(Iterandosobrearray).js
@@ -0,0 +1,16 @@
+// Transforme o código de iteração sobre arrays em uma FUNÇÃO. Lembre-se de chamar a função ao final do código para ela ser executada.
+
+let i = 0;
+const array = [1,2,3,4,5,6,7,8,9,10];
+function interaarray (){
+array.forEach(element => {
+    console.log(array[i]);
+    i++;
+    
+});
+
+}
+
+interaarray()
+
+
