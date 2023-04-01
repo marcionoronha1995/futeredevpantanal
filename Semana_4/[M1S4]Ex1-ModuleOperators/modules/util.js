ba93775134b5de464243667b385fbcac6bdd3d27
Marcio 1995 Noronha <marcionoronha1995@gmail.com>
2023-04-01 19:47:39 -0300

    ddd

---
+++ Semana_4/[M1S4]Ex1-ModuleOperators/modules/util.js
@@ -0,0 +1,17 @@
+// ● conversorReais: converte um número em um valor em reais , ou seja se ela receber 4
+// dever á converter para R$ 4,00
+function conversorReais(){
+    
+}
+
+
+// ● conversorCPF: crie um algoritmo que altere uma string para o formato de CPF
+function conversorCPF(){
+
+}
+
+
+// ● ordenadorArray: função que realiza a organização dos números dentro de um array
+function ordenadorArray(){
+
+}
