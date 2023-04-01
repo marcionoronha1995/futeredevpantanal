81fb41da2d866e50f26c01197eafa447c27ea25f
Marcio 1995 Noronha <marcionoronha1995@gmail.com>
2023-04-01 19:23:32 -0300

    foi o 9

---
+++ Semana_3/[MxSyy]Ex9-Funcoes(Soma de arrays).js
@@ -0,0 +1,51 @@
+// Crie uma função que encapsula o seu programa de somar arrays e utiliza o operador REST para captar os argumentos da função. 
+
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+function parouimpar (ah) {
+
+    const resultado = (ah/2) % 2 == 0
+    let bh
+    var resto = ah % 2;
+
+    if (resto == 0) {
+        bh = "número "+ah+" é par!"
+
+    } else {
+        bh = "número "+ah+" é impar!"
+    };
+    
+    return bh;
+
+};
+
+let i = 0;
+const array = [1,"s",2,"e",3,4,"r",5,6,"t","y","u","eu vi um gatinho","i",7,8,"o","p",9,"q",10,"n"];
+const array1 = ['java',52, 'javascript',101, 'python', 'html', 'css',221,'cobol',332,'fortran','assembler',444,'Objective-C','c'];
+
+function sumarray(a,b){
+    const array2 = [...array,...array1];
+    return array2
+
+}
+const array2 = sumarray(array,array1);
+
+let soma = 0;
+array2.forEach(element => {
+
+    let xyz = array2[i];
+    console.log("soma = ",soma);
+  
+    if (isNumber(array2[i])) {
+        console.log("encontrei um número ",array2[i],". O ",parouimpar(xyz));
+        soma = soma + array2[i];
+        console.log("soma = ",soma);
+    }; 
+    i++; 
+
+
+    
+});
+console.log("O valor total da soma dos números do array é ",soma);
