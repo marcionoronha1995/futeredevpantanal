df9b17c44b3f1b84b10f7d2280978266644d4e15
Marcio1995 <123603516+marcionoronha1995@users.noreply.github.com>
2023-04-15 01:06:39 -0300

    vai que

--- Semana_4/M1S4Ex3-AsyncAwaitFetchAPICEP/script.js
+++ Semana_4/M1S4Ex3-AsyncAwaitFetchAPICEP/script.js
@@ -46,3 +46,22 @@ function informeCEP(){
 //         }
 //     ]
 // }
+
+
+
+// by brasil
+// {
+//     "cep": "89010025",
+//     "state": "SC",
+//     "city": "Blumenau",
+//     "neighborhood": "Centro",
+//     "street": "Rua Doutor Luiz de Freitas Melro",
+//     "service": "viacep",
+//     "location": {
+//       "type": "Point",
+//       "coordinates": {
+//         "longitude": "-49.0629788",
+//         "latitude": "-26.9244749"
+//       }
+//     }
+//   }
