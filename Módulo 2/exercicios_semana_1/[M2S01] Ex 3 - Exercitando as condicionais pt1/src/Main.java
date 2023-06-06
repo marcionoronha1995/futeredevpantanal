542e96165e537552cc2fb31fba0f5a802226ef59
Marcio 1995 Noronha <marcionoronha1995@gmail.com>
2023-06-05 21:37:23 -0300

    uou

---
+++ Módulo 2/exercicios_semana_1/[M2S01] Ex 3 - Exercitando as condicionais pt1/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(" Verificando se você pode votar !");
+
+        System.out.println("Você de informar 3 números!");
+
+        Scanner idade_entrada = new Scanner(System.in);
+        double idade;
+
+        System.out.println("Digite a sua idade: ");
+        idade = idade_entrada.nextInt();
+
+
+
+
+
+
+
+    }
+}
